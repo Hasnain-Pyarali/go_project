e8dbbab4762ad7b1a5706cc832c5de191aed8621
Hasnain-Pyarali <hasnain.pyarali@epitech.eu>
2024-10-20 19:17:05 +0700

    arrays_and_slices
    - User input
    - Store data in slice, instead of arrays, for more flexibility

--- main.go
+++ main.go
@@ -2,6 +2,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your tockets here to attend")
+	var confName = "Go Conference"
+	const totalTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", confName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", totalTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var bookings []string
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter youre first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter youre last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter youre email adress:")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+	bookings = append(bookings, firstName + " " + lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confName)
+	fmt.Printf("These are all of our bookings %v\n", bookings)
 }
